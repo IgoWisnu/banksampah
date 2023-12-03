53f2591004677f8fd312947e1c36194057461334
gdangga <gedeangga424@gmail.com>
2023-12-03 21:18:28 +0800

    dashboard admin

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,21 @@
+<?php
+    
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class dashboard extends CI_Controller {
+    
+        public function index(){
+            $this->load->model('m_dashboard');  // Load the model
+            $data['user'] = $this->m_dashboard->getData();  // Fetch data from the model
+            $data['adminCount'] = $this->m_dashboard->getAdminCount();
+            $data['nasabahCount'] = $this->m_dashboard->getNasabahCount();
+
+    
+            $this->load->view('banksampah/Lindexx', $data);  // Pass data to the view
+        }
+    
+    }
+    
+    /* End of file banksampah.php */
+    
+?>
