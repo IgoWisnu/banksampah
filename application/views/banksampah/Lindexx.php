8a7ceaec41f12e566d4368bcbda44f527f1d903b
gdangga <gedeangga424@gmail.com>
2023-12-28 09:45:19 +0800

    icon

--- application/views/banksampah/Lindexx.php
+++ application/views/banksampah/Lindexx.php
@@ -12,6 +12,7 @@
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css">
             <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+            <link rel="shortcut icon" type="image/x-icon" href="<?=base_url()?>img/logo white.png" />
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
             <title>Admin Dashboard</title>
         </head>
