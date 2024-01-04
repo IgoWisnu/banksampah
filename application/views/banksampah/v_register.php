c213c954780fe0d0a527aa28713604aa1f1633c7
HyRantz21 <sagita.ketut213@gmail.com>
2024-01-04 20:47:09 +0800

    update dikit

--- application/views/banksampah/v_register.php
+++ application/views/banksampah/v_register.php
@@ -8,14 +8,15 @@
 </head>
   
 <style>
-    .Container{
-        background: #557C55;
-        height: 1400px;
+    .background{
+        height: 1500px;
+        background: rgb(0,146,110);
+        background: linear-gradient(0deg, rgba(0,146,110,1) 0%, rgba(0,146,110,1) 20%, rgba(0,146,110,1) 36%, rgba(29,157,131,1) 52%, rgba(75,176,164,1) 78%, rgba(147,205,217,1) 100%);
     }
 
     .Panel{
         width: 370px;
-        height: 1100px; 
+        height: 1000px; 
         background: white;
         border-radius: 30px; 
     }
@@ -64,26 +65,31 @@
     }
 
     .form{
+        left: 25px;
+        justify-content: center;
+        display: flex;
         font-size:   90%;
         font-weight: 500;
     }
 
     .form-control{
+        width: 300px;
         border-width: 2px;
         border-color: black;
         padding: 11px;
         border-radius: 5mm;
     }
     
+    
     .btn{
         border-radius: 5mm;
         top: 25px;
         position: relative;
-        background-color: #557C55;
+        background: rgb(0,146,110);
     }
 
     .btn:hover{
-        background-color: #557C55;
+        background: rgb(0,146,110);
     }
 
     .tbSignUp{
@@ -97,87 +103,85 @@
     }
 
 </style>
-<body>
+<body class="background">
     <form action="<?=base_url('auth/mail') ?>" method="post">
-        <div class="Container col justify-content-center">
-            <div class="row justify-content-center">
-                <div class="SelamKenal">
-                    Salam Kenal
-                </div>
-                <div class="SilahkanIsiDataDiriUntukMenggunakanBankSampah w-75">
-                    Silahkan Isi Data Diri Untuk Menggunakan Bank Sampah
-                </div>
-                <div class="AtauSudahPunyaAkun">
-                    Atau Sudah Punya Akun?<a class="link" href="<?=base_url('auth') ?>">Login</a>
-                </div>
+        <div class="row justify-content-center">
+            <div class="SelamKenal">
+                Salam Kenal
             </div>
-        
-            <div class="row justify-content-center">
-                <div class="Panel">
-                    <div class="tSignUp">SignUp</div>
-                    <div class="form row justify-content-center"> 
-                        <div class="w-75 pb-2">Username:
-                            <input type="text" id="username" class="form-control" name="username" placeholder="Enter your username" value="<?=set_value('username') ?>">
-                            <small class="text-danger"><?= form_error('username') ?></small>
+            <div class="SilahkanIsiDataDiriUntukMenggunakanBankSampah w-75">
+                Silahkan Isi Data Diri Untuk Menggunakan Bank Sampah
+            </div>
+            <div class="AtauSudahPunyaAkun">
+                Atau Sudah Punya Akun?<a class="link" href="<?=base_url('auth') ?>">Login</a>
+            </div>
+        </div>
+    
+        <div class="row justify-content-center">
+            <div class="Panel">
+                <div class="tSignUp">SignUp</div>
+                <div class="form"> 
+                    <div class="pb-2">Username:
+                        <input type="text" id="username" class="form-control row justify-content-center" name="username" placeholder="Enter your username" value="<?=set_value('username') ?>">
+                        <small class="text-danger"><?= form_error('username') ?></small>
+                    </div>
+                </div>
+                <div class="form">
+                    <div class="pb-2">Email:
+                        <input type="email" id="email" class="form-control row justify-content-center" name="email" placeholder="Enter your email address" value="<?=set_value('email') ?>">
+                        <small class="text-danger"><?= form_error('email') ?></small>
+                    </div>
+                </div>
+                <div class="form">
+                    <div class="pb-2">Nama Lengkap:
+                        <input type="text" id="nama_lengkap" class="form-control row justify-content-center" name="nama_lengkap" placeholder="Enter your full name" value="<?=set_value('nama_lengkap') ?>">
+                        <small class="text-danger"><?= form_error('nama_lengkap') ?></small>
+                    </div>
+                </div>
+                <div class="form">
+                    <div class="pb-2">Tempat Lahir:
+                        <input type="text" id="tempat_lahir" class="form-control row justify-content-center" name="tempat_lahir" placeholder="Enter your place of birth" value="<?=set_value('tempat_lahir') ?>">
+                        <small class="text-danger"><?= form_error('tempat_lahir') ?></small>
+                    </div>
+                </div>
+                <div class="form">
+                    <div class="pb-2">Tanggal Lahir:
+                        <input type="date" id="tanggal_lahir" class="form-control row justify-content-center" name="tanggal_lahir" placeholder="Enter your date of birth" value="<?=set_value('tanggal_lahir') ?>">
+                        <small class="text-danger"><?= form_error('tanggal_lahir') ?></small>
+                    </div>
+                </div>
+                <div class="form">
+                    <div class="pb-2">Alamat:
+                        <input type="text" id="alamat" class="form-control row justify-content-center" name="alamat" placeholder="Enter your address" value="<?=set_value('alamat') ?>">
+                        <small class="text-danger"><?= form_error('alamat') ?></small>
+                    </div>
+                </div>
+                <div class="form">
+                    <div class="pb-2">No Telp:
+                        <input type="text" id="notelp" class="form-control row justify-content-center" name="notelp" placeholder="Enter your phone number" value="<?=set_value('notelp') ?>">
+                        <small class="text-danger"><?= form_error('notelp') ?></small>
                         </div>
                     </div>
-                    <div class="form row justify-content-center">
-                        <div class="w-75 pb-2">Email:
-                            <input type="email" id="email" class="form-control" name="email" placeholder="Enter your email address" value="<?=set_value('email') ?>">
-                            <small class="text-danger"><?= form_error('email') ?></small>
-                        </div>
+                <div class="form">
+                    <div class=" pb-2">Password:
+                        <input type="password" id="password" class="form-control row justify-content-center" name="password" placeholder="Enter your password">
+                        <small class="text-danger"><?= form_error('password') ?></small>
                     </div>
-                    <div class="form row justify-content-center">
-                        <div class="w-75 pb-2">Nama Lengkap:
-                            <input type="text" id="nama_lengkap" class="form-control" name="nama_lengkap" placeholder="Enter your full name" value="<?=set_value('nama_lengkap') ?>">
-                            <small class="text-danger"><?= form_error('nama_lengkap') ?></small>
-                        </div>
+                </div>
+                <div class="form">
+                    <div class=">Verify Password:
+                        <input type="password" id="verify_password" class="form-control row justify-content-center" name="verify_password" placeholder="Re-enter your password" >
+                        <small class="text-danger"><?= form_error('verify_password') ?></small>
                     </div>
-                    <div class="form row justify-content-center">
-                        <div class="w-75 pb-2">Tempat Lahir:
-                            <input type="text" id="tempat_lahir" class="form-control" name="tempat_lahir" placeholder="Enter your place of birth" value="<?=set_value('tempat_lahir') ?>">
-                            <small class="text-danger"><?= form_error('tempat_lahir') ?></small>
-                        </div>
+                </div>
+                <div class="form row justify-content-center">
+                    <div class="form-check ps-5 pe-5 pt-4 ms-4">
+                        <input class="form-check-input" type="checkbox" id="agreementCheck">
+                        Saya menyetujui semua ketentuan yang berlaku
                     </div>
-                    <div class="form row justify-content-center">
-                        <div class="w-75 pb-2">Tanggal Lahir:
-                            <input type="date" id="tanggal_lahir" class="form-control" name="tanggal_lahir" placeholder="Enter your date of birth" value="<?=set_value('tanggal_lahir') ?>">
-                            <small class="text-danger"><?= form_error('tanggal_lahir') ?></small>
-                        </div>
-                    </div>
-                    <div class="form row justify-content-center">
-                        <div class="w-75 pb-2">Alamat:
-                            <input type="text" id="alamat" class="form-control" name="alamat" placeholder="Enter your address" value="<?=set_value('alamat') ?>">
-                            <small class="text-danger"><?= form_error('alamat') ?></small>
-                        </div>
-                    </div>
-                    <div class="form row justify-content-center">
-                        <div class="w-75 pb-2">No Telp:
-                            <input type="text" id="notelp" class="form-control" name="notelp" placeholder="Enter your phone number" value="<?=set_value('notelp') ?>">
-                            <small class="text-danger"><?= form_error('notelp') ?></small>
-                            </div>
-                        </div>
-                    <div class="form row justify-content-center">
-                        <div class=" w-75 pb-2">Password:
-                            <input type="password" id="password" class="form-control" name="password" placeholder="Enter your password">
-                            <small class="text-danger"><?= form_error('password') ?></small>
-                        </div>
-                    </div>
-                    <div class="form row justify-content-center">
-                        <div class="w-75">Verify Password:
-                            <input type="password" id="verify_password" class="form-control" name="verify_password" placeholder="Re-enter your password" >
-                            <small class="text-danger"><?= form_error('verify_password') ?></small>
-                        </div>
-                    </div>
-                    <div class="form row justify-content-center">
-                        <div class="form-check ps-5 pe-5 pt-5">
-                            <input class="form-check-input" type="checkbox" id="agreementCheck">
-                            Saya menyetujui semua ketentuan yang berlaku
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <input type="submit" class="tbSignUp btn w-50" value="SignUp"></input>
-                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <input type="submit" class="tbSignUp btn w-50" value="SignUp"></input>
                 </div>
             </div>
         </div>
